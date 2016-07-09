1c5b6af27f432b4a005d880f24247f440fb089c3
Inflex <pldaniels@gmail.com>
2016-07-09 10:08:47 +1000

    Code cleanup and comments for DX9 Windows

--- src/openboardview/main_dx9.cpp
+++ src/openboardview/main_dx9.cpp
@@ -1,6 +1,12 @@
-// ImGui - standalone example application for DirectX 9
-// If you are new to ImGui, see examples/README.txt and documentation at the top of imgui.cpp.
-
+/**
+ * Open[Flex] Board View
+ *
+ * Copyright chloridite 2016
+ * Copyright inflex 2016 (Paul Daniels)
+ *
+ * Git Fork: https://github.com/inflex/OpenBoardView
+ *
+ */
 #include "BoardView.h"
 
 #include "imgui_impl_dx9.h"
@@ -44,7 +50,7 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 	// Initialize comctl
 	CoInitializeEx(NULL, COINIT_MULTITHREADED);
 
-	static const wchar_t *class_name = L"ImGui Example";
+	static const wchar_t *class_name = L"Openflex Board View";
 
 	// Create application window
 	HINSTANCE instance = GetModuleHandle(NULL);
@@ -52,7 +58,7 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 	WNDCLASSEX wc      = {sizeof(WNDCLASSEX), CS_CLASSDC, WndProc, 0L, 0L, instance, icon, NULL, NULL, NULL, class_name, NULL};
 	RegisterClassEx(&wc);
 	HWND hwnd = CreateWindow(class_name,
-	                         _T("Open Board Viewer"),
+	                         _T("Openflex Board Viewer"),
 	                         WS_OVERLAPPEDWINDOW,
 	                         CW_USEDEFAULT,
 	                         CW_USEDEFAULT,
@@ -115,6 +121,7 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 	_CrtSetDbgFlag(tmpFlag);
 #endif
 	BoardView app{};
+	app.History_load();
 
 	bool show_test_window    = true;
 	bool show_another_window = false;
@@ -132,39 +139,6 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 			continue;
 		}
 		ImGui_ImplDX9_NewFrame();
-#if 0
-		// 1. Show a simple window
-		// Tip: if we don't call ImGui::Begin()/ImGui::End() the widgets appears in a window
-		// automatically called "Debug"
-		{
-			static float f = 0.0f;
-			ImGui::Text("Hello, world!");
-			ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
-			ImGui::ColorEdit3("clear color", (float *)&clear_col);
-			if (ImGui::Button("Test Window"))
-				show_test_window ^= 1;
-			if (ImGui::Button("Another Window"))
-				show_another_window ^= 1;
-			ImGui::Text("Application average %.3f ms/frame (%.1f FPS)",
-			            1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-		}
-
-		// 2. Show another simple window, this time using an explicit Begin/End pair
-		if (show_another_window) {
-			ImGui::SetNextWindowSize(ImVec2(200, 100), ImGuiSetCond_FirstUseEver);
-			ImGui::Begin("Another Window", &show_another_window);
-			ImGui::Text("Hello");
-			ImGui::End();
-		}
-
-#endif
-#if 0
-		// 3. Show the ImGui test window. Most of the sample code is in ImGui::ShowTestWindow()
-		if (show_test_window) {
-			ImGui::SetNextWindowPos(ImVec2(650, 20), ImGuiSetCond_FirstUseEver);
-			ImGui::ShowTestWindow(&show_test_window);
-		}
-#endif
 		app.Update();
 		if (app.m_wantsQuit) {
 			PostMessage(hwnd, WM_QUIT, 0, 0);
