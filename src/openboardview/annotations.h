671a907dcb3d382f9b68e68181286170aa8e75cf
Inflex <pldaniels@gmail.com>
2016-07-17 13:13:38 +1000

    Prepped file for annotations (future feature)

--- src/openboardview/annotations.h
+++ src/openboardview/annotations.h
@@ -1,5 +1,23 @@
 
 struct annotations {
+
+#define ANNOTATION_NET_SIZE 256
+#define ANNOTATION_PART_SIZE 256
+#define ANNOTATION_FNAME_LEN_MAX 2048
+	struct annotation {
+		char net[ANNOTATION_NET_SIZE];
+		char part[ANNOTATION_PART_SIZE];
+		double x;
+		double y;
+		int side;
+		char *comment;
+	};
+
+	struct annotations {
+		char fname[ANNOTATION_FNAME_LEN_MAX];
+		// vector of struct annotation
+	};
+
 	int Annotations_set_filename(char *f);
 	int Annotations_load(void);
 	int Annotations_save(void);
