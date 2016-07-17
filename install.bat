943cb7c8c946a175cfa66e9289df005d87b964ad
Inflex <pldaniels@gmail.com>
2016-07-17 18:45:14 +1000

    Simple windows batch installer finished (for now)

--- install.bat
+++ install.bat
@@ -1,56 +1,57 @@
 @echo off
-echo "Installing OpenFlex Board View"
+echo Installing OpenFlex Board View
 
 
-echo "Configuration setup"
+echo Configuration setup
 set OD=%APPDATA%\openboardview
 IF exist "%OD%" (
-		echo "Config install path already exists"
+		echo Config install path already exists
 ) else (
-		echo "Creating config installation path %OD%..."
+		echo Creating config installation path %OD%...
 		mkdir "%OD%"
 )
 set ODC=%OD%\obv.conf
 IF exist "%ODC%" (
-	echo "Configuration file already exists, not overwriting"
+	echo Configuration file already exists, not overwriting
 ) else (
-	echo "Copying configuration file..."
+	echo Copying configuration file...
 	copy sample-obv.conf "%ODC%"
 )
 
 
 
-set PD=%HOMEPATH%\openboardview
+set PD=%HOMEDRIVE%%HOMEPATH%\openboardview
 IF exist "%PD%" (
-		echo "Binary install path already exists"
+		echo Binary install path already exists
 ) else (
-		echo "Creating binary installation path %PD%..."
+		echo Creating binary installation path %PD%...
 		mkdir "%PD%"
 )
-echo "Copying binary...
+echo Copying binary...
 copy openboardview.exe "%PD%"
 
 
-
 set LNK=%HOMEDRIVE%%HOMEPATH%\Desktop\openboardview.lnk
-if exist "%LNK%" (
-		echo "Desktop link already exists"
-		) else (
 
 echo Set oWS = WScript.CreateObject("WScript.Shell") > CreateShortcut.vbs
 echo sLinkFile = "%LNK%" >> CreateShortcut.vbs
 echo Set oLink = oWS.CreateShortcut(sLinkFile) >> CreateShortcut.vbs
-echo oLink.TargetPath = "%PD%" >> CreateShortcut.vbs
+echo oLink.TargetPath = "%PD%\openboardview.exe" >> CreateShortcut.vbs
 echo oLink.Save >> CreateShortcut.vbs
 
-echo "Creating desktop link..."
-cscript CreateShortcut.vbs
-;;del CreateShortcut.vbs
+if exist "%LNK%" (
+	echo Desktop link already exists
+) else (
+	echo Creating desktop link...
+	cscript CreateShortcut.vbs
 )
 
-
-echo "Install finished."
-echo "You can now run the binary at %PD%"
-
-echo
+echo .
+echo .
+echo Install finished.
+echo You can now run the binary at %PD%\openboardview.exe
+echo Check for a link on the desktop too.
+echo .
+echo .
+@echo on
 
