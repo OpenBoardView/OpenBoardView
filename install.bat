98a8fb7ee3aca4669c981d5569f19ec290085501
Inflex <pldaniels@gmail.com>
2016-07-17 18:27:45 +1000

    Changes made to install.bat, making OBV install in to %HOMEPATH%\openboardview rather than %PROGRAMS% because the latter requires admin priviledges

--- install.bat
+++ install.bat
@@ -1,72 +1,56 @@
-
+@echo off
 echo "Installing OpenFlex Board View"
 
-IF exist openboardview.exe (
-IF exist sample-obv.conf (
 
-
-
-;; Create the configuration and history file paths
-;;
+echo "Configuration setup"
 set OD=%APPDATA%\openboardview
-IF exist %OD% (
+IF exist "%OD%" (
 		echo "Config install path already exists"
 ) else (
 		echo "Creating config installation path %OD%..."
-		mkdir %OD%
+		mkdir "%OD%"
 )
-IF exist %OD%\obv.conf (
+set ODC=%OD%\obv.conf
+IF exist "%ODC%" (
 	echo "Configuration file already exists, not overwriting"
 ) else (
 	echo "Copying configuration file..."
-	copy sample-obv.conf %OD%\obv.conf
+	copy sample-obv.conf "%ODC%"
 )
 
 
 
-;; Create the binary install path
-;;
-set PD=%PROGRAMFILES%\openboardview
-IF exist %PD% (
+set PD=%HOMEPATH%\openboardview
+IF exist "%PD%" (
 		echo "Binary install path already exists"
 ) else (
 		echo "Creating binary installation path %PD%..."
-		mkdir %PD%
+		mkdir "%PD%"
 )
 echo "Copying binary...
-copy openboardview.exe %PD%
+copy openboardview.exe "%PD%"
 
 
 
-;; Attempt to create a desktop link
-;;
-set LNK="%HOMEDRIVE%%HOMEPATH%\Desktop\openboardview.lnk"
-if exist %LNK% (
+set LNK=%HOMEDRIVE%%HOMEPATH%\Desktop\openboardview.lnk
+if exist "%LNK%" (
 		echo "Desktop link already exists"
 		) else (
-			if exist %PD% (
-@echo off
+
 echo Set oWS = WScript.CreateObject("WScript.Shell") > CreateShortcut.vbs
-echo sLinkFile = "%HOMEDRIVE%%HOMEPATH%\Desktop\openboardview.lnk" >> CreateShortcut.vbs
+echo sLinkFile = "%LNK%" >> CreateShortcut.vbs
 echo Set oLink = oWS.CreateShortcut(sLinkFile) >> CreateShortcut.vbs
 echo oLink.TargetPath = "%PD%" >> CreateShortcut.vbs
 echo oLink.Save >> CreateShortcut.vbs
-@echo on
-		echo "Creating desktop link..."
+
+echo "Creating desktop link..."
 cscript CreateShortcut.vbs
-del CreateShortcut.vbs
-)
+;;del CreateShortcut.vbs
 )
 
 
 echo "Install finished."
-echo "You can now run the binary at %OD%\openboardview.exe"
-) else (
-	echo "Cannot find sample-obv.conf to install."
-	)
-) else (
-	echo "Cannot find openboardview.exe to install."
-	)
+echo "You can now run the binary at %PD%"
 
 echo
 
