43a72e85e7cc648143ccd957c4f90916723af274
Inflex <pldaniels@gmail.com>
2016-07-23 00:13:11 +1000

    Updated help-Controls to reflect new toggle keys for FPS, position, and pin blanking

--- src/openboardview/BoardView.cpp
+++ src/openboardview/BoardView.cpp
@@ -283,7 +283,16 @@ void BoardView::HelpControls(void) {
 		ImGui::Text("Reset zoom and center");
 		ImGui::Text("Rotate clockwise");
 		ImGui::Text("Rotate anticlockwise");
+		ImGui::Spacing();
+		ImGui::Spacing();
 
+		ImGui::Text("Toggle Pin Blanking");
+		ImGui::Text("Toggle FPS display");
+		ImGui::Text("Toggle Position");
+
+		/*
+		 * NEXT COLUMN
+		 */
 		ImGui::NextColumn();
 		ImGui::Text("CTRL-o");
 		ImGui::Text("Ctrl-q");
@@ -316,6 +325,13 @@ void BoardView::HelpControls(void) {
 		ImGui::Text("x, numpad-5");
 		ImGui::Text("'.' numpad '.'");
 		ImGui::Text("',' numpad-0");
+		ImGui::Spacing();
+		ImGui::Spacing();
+
+		ImGui::Text("b");
+		ImGui::Text("f");
+		ImGui::Text("p");
+
 		ImGui::Columns(1);
 		ImGui::Separator();
 
