56355c1e42d4d03835d3f4a65874734fb00e640f
piernov <piernov@piernov.org>
2016-07-28 16:35:24 +0200

    Fix loading of file without ext

--- src/openboardview/BoardView.cpp
+++ src/openboardview/BoardView.cpp
@@ -237,7 +237,12 @@ int BoardView::detectFiletype(char *buf) {
 int BoardView::LoadFile(char *filename) {
 	if (filename) {
 		char *ext = strrchr(filename, '.');
-		for (int i = 0; ext[i]; i++) ext[i] = tolower(ext[i]); // Convert extension to lowercase
+		if (ext) {
+			for (int i = 0; ext[i]; i++) ext[i] = tolower(ext[i]); // Convert extension to lowercase
+		} else {
+			ext = strrchr(filename, '\0'); // No extension, point to end of filename
+		}
+
 		SetLastFileOpenName(filename);
 		size_t buffer_size;
 		char *buffer = file_as_buffer(&buffer_size, filename);
