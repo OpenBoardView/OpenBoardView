813cf87e09f4fb7f6f11efbbeb75993967aebdaf
mrxordi <xordi@wp.pl>
2016-08-24 12:58:18 +0200

    GetFontData in some cases returns GDI_ERROR Fixes OpenBoardView/OpenBoardView/#42

--- src/openboardview/win32.cpp
+++ src/openboardview/win32.cpp
@@ -109,7 +109,7 @@ const std::vector<char> load_font(const std::string &name) {
 			return data;
 
 		const size_t size = ::GetFontData(hdc, 0, 0, NULL, 0);
-		if (size > 0) {
+		if (size > 0 && size != GDI_ERROR) {
 			char *buffer = new char[size];
 			if (::GetFontData(hdc, 0, 0, buffer, size) == size) {
 				data = std::vector<char>(buffer, buffer + size);
