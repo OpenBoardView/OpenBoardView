a6c2a231efd852666fcee443eddc5174a5a6269c
Samuel Rakitničan <samuel.rakitnican@gmail.com>
2017-07-24 22:37:15 +0200

    build.sh: Rename function: color it is

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-fmtText() {
+color() {
   color="$1"
   text="$2"
   echo "$(tput bold; tput setaf ${color})${text}$(tput sgr0)"
@@ -8,9 +8,9 @@ fmtText() {
 
 helpMsg() {
   cat << EOH
-Usage: $(fmtText 4 ${0}) [--$(fmtText 5 recompile)] [--$(fmtText 1 debug)] — Build $PROJECT
-          --$(fmtText 5 recompile)   — Delete $(fmtText 6 \$COMPILEDIR) (release_build or debug_build with --$(fmtText 1 debug)) before compiling $PROJECT again
-          --$(fmtText 1 debug)       — Make a $(fmtText 1 debug) build
+Usage: $(color 4 ${0}) [--$(color 5 recompile)] [--$(color 1 debug)] — Build $PROJECT
+          --$(color 5 recompile)   — Delete $(color 6 \$COMPILEDIR) (release_build or debug_build with --$(color 1 debug)) before compiling $PROJECT again
+          --$(color 1 debug)       — Make a $(color 1 debug) build
 
 All extra parameters are passed to cmake.
 Environment variables:
@@ -18,7 +18,7 @@ Environment variables:
 EOH
 }
 
-PROJECT="$(fmtText 3 OpenBoardView)"
+PROJECT="$(color 3 OpenBoardView)"
 if [ -z $THREADS ]; then
     THREADS=1
     case "$(uname -s)" in
@@ -37,20 +37,20 @@ if [ "$1" == "--help" ]; then
   helpMsg
   exit
 fi
-STRCOMPILE="$(fmtText 2 Compiling)"
+STRCOMPILE="$(color 2 Compiling)"
 COMPILEDIR="release_build"
 COMPILEFLAGS="-DCMAKE_INSTALL_PREFIX="
 export DESTDIR="$(cd "$(dirname "$0")" && pwd)"
-BUILDTYPE="$(fmtText 6 release)"
+BUILDTYPE="$(color 6 release)"
 SCRIPT_ARGC=1 # number of arguments eaten by this script
 if [ "$ARG_LENGTH" -gt 0 -a "$1" == "--debug" -o "$2" == "--debug" ]; then
   COMPILEDIR="debug_build"
   COMPILEFLAGS="$COMPILEFLAGS -DCMAKE_BUILD_TYPE=DEBUG"
-  BUILDTYPE="$(fmtText 1 debug)"
+  BUILDTYPE="$(color 1 debug)"
   SCRIPT_ARGC=$((SCRIPT_ARGC+1))
 fi
 if [ "$ARG_LENGTH" -gt 0 -a "$1" == "--recompile" -o "$2" == "--recompile" ]; then
-  STRCOMPILE="$(fmtText 5 Recompiling)"
+  STRCOMPILE="$(color 5 Recompiling)"
   rm -rf $COMPILEDIR
   SCRIPT_ARGC=$((SCRIPT_ARGC+1))
 fi
@@ -59,7 +59,7 @@ if [ "$CROSS" == "mingw64" ]; then
 fi
 COMPILEFLAGS="$COMPILEFLAGS ${@:${SCRIPT_ARGC}}" # pass other arguments to CMAKE
 if [ $THREADS -lt 1 ]; then
-  fmtText 1 "Unable to detect number of threads, using 1 thread."
+  color 1 "Unable to detect number of threads, using 1 thread."
   THREADS=1
 fi
 if [ ! -d $COMPILEDIR ]; then
@@ -72,23 +72,23 @@ if [ $THREADS -eq 1 ]; then
 fi
 
 # Now compile the source code and install it in server's directory
-echo "$STRCOMPILE $PROJECT using $(fmtText 4 $THREADS) $STRTHREADS ($BUILDTYPE build)"
+echo "$STRCOMPILE $PROJECT using $(color 4 $THREADS) $STRTHREADS ($BUILDTYPE build)"
 echo "Extra flags passed to CMake: $COMPILEFLAGS"
 cmake $COMPILEFLAGS ..
-[[ "$?" != "0" ]] && fmtText 1 "CMAKE FAILED" && exit 1
+[[ "$?" != "0" ]] && color 1 "CMAKE FAILED" && exit 1
 if `echo "$COMPILEFLAGS" | grep -q "DEBUG"`; then
   make -j$THREADS install
-  [[ "$?" != "0" ]] && fmtText 1 "MAKE INSTALL FAILED" && exit 1
+  [[ "$?" != "0" ]] && color 1 "MAKE INSTALL FAILED" && exit 1
 else
   make -j$THREADS install/strip
-  [[ "$?" != "0" ]] && fmtText 1 "MAKE INSTALL/STRIP FAILED" && exit 1
+  [[ "$?" != "0" ]] && color 1 "MAKE INSTALL/STRIP FAILED" && exit 1
 fi
 
 case "$(uname -s)" in
   *Darwin*)
     # Generate DMG
     make package
-    [[ "$?" != "0" ]] && fmtText 1 "MAKE PACKAGE FAILED" && exit 1
+    [[ "$?" != "0" ]] && color 1 "MAKE PACKAGE FAILED" && exit 1
     ;;
   *)
     # Give right execution permissions to executables
