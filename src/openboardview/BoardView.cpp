53da8645c8b53bbe9e1bec2c0decfe78758eec18
Matt Sturgeon <matt@sturgeon.me.uk>
2017-08-15 13:58:26 +0100

    Use LICENSE file for the about page
    List LICENSE and LICENSE_TEXT variables in version.cmake. LICENSE_TEXT
    is read from the contents of the main LICENSE file and later used in
    BoardView.cpp to print the About dialogue.

--- src/openboardview/BoardView.cpp
+++ src/openboardview/BoardView.cpp
@@ -937,45 +937,8 @@ void BoardView::HelpAbout(void) {
 		ImGui::Dummy(ImVec2(1, DPI(10)));
 		ImGui::Text("License info");
 		ImGui::Separator();
-		ImGui::Text(OBV_NAME " is MIT Licensed");
-		ImGui::Text("Copyright (c) 2016 Inflex (Paul Daniels)");
-		ImGui::Text("Copyright (c) 2016 Chloridite (original source)");
-		ImGui::Spacing();
-		ImGui::Dummy(ImVec2(1, DPI(10)));
-		ImGui::Text("ImGui is MIT Licensed");
-		ImGui::Text("Copyright (c) 2014-2015 Omar Cornut and ImGui contributors");
-		ImGui::Separator();
-		ImGui::Text("The MIT License");
-		ImGui::TextWrapped(
-		    "Permission is hereby granted, free of charge, to any person "
-		    "obtaining a copy "
-		    "of this software and associated documentation files (the "
-		    "\"Software\"), to deal "
-		    "in the Software without restriction, including without limitation "
-		    "the rights "
-		    "to use, copy, modify, merge, publish, distribute, sublicense, "
-		    "and/or sell "
-		    "copies of the Software, and to permit persons to whom the Software "
-		    "is "
-		    "furnished to do so, subject to the following conditions: ");
-		ImGui::TextWrapped(
-		    "The above copyright notice and this permission "
-		    "notice shall be included in all "
-		    "copies or substantial portions of the Software.");
-		ImGui::TextWrapped(
-		    "THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY "
-		    "OF ANY KIND, EXPRESS OR "
-		    "IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES "
-		    "OF MERCHANTABILITY, "
-		    "FITNESS FOR A PARTICULAR PURPOSE AND "
-		    "NONINFRINGEMENT. IN NO EVENT SHALL THE "
-		    "AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY "
-		    "CLAIM, DAMAGES OR OTHER "
-		    "LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR "
-		    "OTHERWISE, ARISING FROM, "
-		    "OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE "
-		    "OR OTHER DEALINGS IN THE "
-		    "SOFTWARE.");
+		ImGui::TextWrapped(OBV_LICENSE_TEXT);
+
 		ImGui::EndPopup();
 	}
 
