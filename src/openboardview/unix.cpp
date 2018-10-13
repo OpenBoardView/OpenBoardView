b5966ef5ec603d7318ef0aad4143bacf63eea145
David Mell <zraexy@users.noreply.github.com>
2018-10-13 13:34:07 -0800

    Fix get_user_dir for Linux when XDG env var is set

--- src/openboardview/unix.cpp
+++ src/openboardview/unix.cpp
@@ -232,6 +232,8 @@ const std::string get_user_dir(const UserDir userdir) {
 			else if (userdir == UserDir::Data)
 				path += "/.local/share";
 		}
+	} else {
+		path += std::string(envVar);
 	}
 	if (!path.empty()) {
 		path += "/" OBV_NAME "/";
