f5bee05568b6e3fd934dfcca7c118eb659b34549
piernov <piernov@piernov.org>
2020-05-06 18:43:50 +0200

    Drop the 'R' in version numbering
    Having a letter as first character conflicts with most version numbering systems

--- version.cmake
+++ version.cmake
@@ -2,7 +2,7 @@
 
 project(OpenBoardView)
 string(TOLOWER ${PROJECT_NAME} PROJECT_NAME_LOWER)
-set(PROJECT_VERSION "R7.4")
+set(PROJECT_VERSION "7.4")
 set(PROJECT_URL "http://openboardview.org")
 set(PROJECT_LICENSE "MIT")
 file(READ "LICENSE" PROJECT_LICENSE_TEXT)
