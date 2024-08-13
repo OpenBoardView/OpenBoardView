de5c30f5f8d018c7c7e11f8112ec32ad126f6393
piernov <piernov@piernov.org>
2024-08-13 23:54:44 +0200

    Add MenuItemWithCheckbox() to GUI/widgets.cpp

--- src/openboardview/GUI/widgets.cpp
+++ src/openboardview/GUI/widgets.cpp
@@ -1,5 +1,7 @@
 #include "widgets.h"
 
+#include <string>
+
 #include "imgui/imgui.h"
 
 void RightAlignedText(const char *t, int w) {
@@ -8,4 +10,38 @@ void RightAlignedText(const char *t, int w) {
 	ImGui::Dummy(s);
 	ImGui::SameLine();
 	ImGui::Text("%s", t);
+}
+
+bool MenuItemWithCheckbox(const std::string &label, const std::string &shortcut, bool &selected, bool enabled) {
+	ImGuiStyle &style = ImGui::GetStyle();
+
+	// MenuItem is never "selected", value is toggled at the end and Checkbox is used instead to display a checkmark
+	bool ret = ImGui::MenuItem(label.c_str(), shortcut.c_str(), false, enabled);
+	bool hovered = ImGui::IsItemHovered();
+
+
+	ImGui::SameLine();
+
+	// Checkbox will be colored when MenuItem is hovered
+	if (hovered) {
+		ImGui::PushStyleColor(ImGuiCol_FrameBg, style.Colors[ImGuiCol_FrameBgHovered]);
+	}
+
+	// No padding for checkbox to fit in MenuItem height
+	ImGui::PushStyleVar(ImGuiStyleVar_FramePadding, ImVec2(0, 0));
+
+	std::string id = "##C" + label;
+	ImGui::Checkbox(id.c_str(), &selected);
+
+	if (hovered) {
+		ImGui::PopStyleColor();
+	}
+
+	ImGui::PopStyleVar();
+
+	if (ret) {
+		selected = !selected;
+	}
+
+	return ret;
 }
--- src/openboardview/GUI/widgets.h
+++ src/openboardview/GUI/widgets.h
@@ -1,6 +1,9 @@
 #ifndef _WIDGETS_H_
 #define _WIDGETS_H_
 
+#include <string>
+
 void RightAlignedText(const char *t, int w);
+bool MenuItemWithCheckbox(const std::string &label, const std::string &shortcut, bool &selected, bool enabled = true);
 
 #endif//_WIDGETS_H_
