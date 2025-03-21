ac72956dfb4a92660a36db17fc27ba182cb75684
Thomas Lamy <thomas.lamy@iventuregroup.com>
2025-03-21 14:47:07 +0100

    feat: XZZ file format
    99% of this was done by @slimeinacloak. As his time is rather limited, we arranged to have me take over the PR and push it forward

--- src/openboardview/FileFormats/XZZPCBFile.h
+++ src/openboardview/FileFormats/XZZPCBFile.h
@@ -20,7 +20,6 @@ struct XZZPCBFile : public BRDFileBase {
 	int diode_readings_type = 0; // 0 = No readings, 1 = Based on part name and pin name, 2 = Based on net
 
 	// DES
-	uint64_t des(uint64_t input, uint64_t key, char mode);
 	void des_decrypt(std::vector<char> &buf);
     void decryptWithDES(std::vector<char> &buf);
 
@@ -33,11 +32,11 @@ struct XZZPCBFile : public BRDFileBase {
 	void parse_net_block(std::vector<char> &buf);
 	void process_block(uint8_t block_type, std::vector<char> &block_buf);
 
-    char read_utf8_char(char c);
+	char read_utf8_char(char c) const;
 	std::string read_cb2312_string(const std::string &str);
 
 	void find_xy_translation();
     void translate_segments();
-	void translate_points(BRDPoint &point);
+	void translate_points(BRDPoint &point) const;
 	void translate_pins();
 };
