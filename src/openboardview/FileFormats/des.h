4594836200bca88f321e0d706b8e912095766595
Thomas Lamy <thomas.lamy@iventuregroup.com>
2025-03-21 15:10:49 +0100

    fix: replaced auto generated file header with proper source

--- src/openboardview/FileFormats/des.h
+++ src/openboardview/FileFormats/des.h
@@ -1,7 +1,15 @@
-//
-// Created by Thomas Lamy on 21.03.25.
-//
-
+/*
+ * Data Encryption Standard
+ * An approach to DES algorithm
+ *
+ * By: Daniel Huertas Gonzalez
+ * Email: huertas.dani@gmail.com
+ * Version: 0.1
+ *
+ * Based on the document FIPS PUB 46-3
+ *
+ * Source: https://github.com/dhuertas/DES
+ */
 #ifndef DES_H
 #define DES_H
 
@@ -9,13 +17,13 @@
 extern "C" {
 #endif
 
-	/*
-	 * The DES function
-	 * input: 64 bit message
-	 * key: 64 bit key for encryption/decryption
-	 * mode: 'e' = encryption; 'd' = decryption
-	 */
-	uint64_t des(uint64_t input, uint64_t key, char mode);
+/*
+ * The DES function
+ * input: 64 bit message
+ * key: 64 bit key for encryption/decryption
+ * mode: 'e' = encryption; 'd' = decryption
+ */
+uint64_t des(uint64_t input, uint64_t key, char mode);
 
 #ifdef __cplusplus
 }
